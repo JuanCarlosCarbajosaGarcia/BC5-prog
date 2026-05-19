869eb2e56ee94c3746bec2f3b9bf21ab76c4ef2f
JuanCarlosCarbajosaGarcia <juanc.cargar@educa.jcyl.es>
2026-05-19 13:17:44 +0200

    V0.8

--- src/main/java/org/example/EJ10.java
+++ src/main/java/org/example/EJ10.java
@@ -3,6 +3,7 @@ package org.example;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -11,17 +12,18 @@ public class EJ10 {
         Stage ej_10 = new Stage();
 
         ComboBox comboBox = new ComboBox();
-        comboBox.addEventHandler("saludo","hola");
-        comboBox.addEventHandler("despedida","adios");
+        comboBox.getItems().addAll("A","B","C","D");
         Button button = new Button("Boton");
+        Label label = new Label();
 
-        button.setOnAction(boton -> comboBox);
+        button.setOnAction(boton -> label.setText("letra selecionada: " + comboBox.getValue().toString()));
 
         GridPane gridPane = new GridPane();
         gridPane.setHgap(10);
         gridPane.setVgap(10);
         gridPane.add(comboBox,0,0);
         gridPane.add(button,1,0);
+        gridPane.add(label,2,0);
         Scene scene = new Scene(gridPane,300,200);
         ej_10.setTitle("Ejercicio 10");
         ej_10.setScene(scene);
