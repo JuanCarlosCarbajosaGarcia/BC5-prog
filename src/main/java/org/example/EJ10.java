3c67e6c0fc7b6d98026fdc5e33ea635f0ea293c4
JuanCarlosCarbajosaGarcia <juanc.cargar@educa.jcyl.es>
2026-05-19 12:53:47 +0200

    V0.7

--- src/main/java/org/example/EJ10.java
+++ src/main/java/org/example/EJ10.java
@@ -1,13 +1,30 @@
 package org.example;
 
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class EJ10 {
     public static void ejercicio_10(){
         Stage ej_10 = new Stage();
 
+        ComboBox comboBox = new ComboBox();
+        comboBox.addEventHandler("saludo","hola");
+        comboBox.addEventHandler("despedida","adios");
         Button button = new Button("Boton");
 
+        button.setOnAction(boton -> comboBox);
+
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.add(comboBox,0,0);
+        gridPane.add(button,1,0);
+        Scene scene = new Scene(gridPane,300,200);
+        ej_10.setTitle("Ejercicio 10");
+        ej_10.setScene(scene);
+        ej_10.show();
     }
 }
